d95a88c2a5114c728b75462bdf29480f1ede91db
jingwangian <jingwangian@gmail.com>
2019-12-02 17:00:50 +1100

    add jwt

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 import logging
 import connexion
 from flask_cors import CORS
+import jwt
+import time
 
 logging.basicConfig(level=logging.INFO)
 
@@ -9,6 +11,43 @@ logging.basicConfig(level=logging.INFO)
 def hello():
     print('hello: Welcome to related-media server')
     return 'Welcome to related-media server'
+
+
+JWT_ISSUER = 'com.zalando.connexion'
+JWT_SECRET = 'change_this'
+JWT_LIFETIME_SECONDS = 600
+JWT_ALGORITHM = 'HS256'
+
+
+def generate_token(user_id):
+    timestamp = _current_timestamp()
+    payload = {
+        "iss": JWT_ISSUER,
+        "iat": int(timestamp),
+        "exp": int(timestamp + JWT_LIFETIME_SECONDS),
+        "sub": str(user_id),
+    }
+
+    return jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+
+def decode_token(token):
+    try:
+        return jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+    except jwt.exceptions.InvalidTokenError as e:
+        # six.raise_from(Unauthorized, e)
+        raise Exception(f'Invalid Token:{e}')
+
+
+def get_secret(user, token_info) -> str:
+    return '''
+    You are user_id {user} and the secret is 'wbevuec'.
+    Decoded token claims: {token_info}.
+    '''.format(user=user, token_info=token_info)
+
+
+def _current_timestamp() -> int:
+    return int(time.time())
 
 
 if __name__ == '__main__':
