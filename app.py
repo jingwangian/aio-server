c1a092ad4a3fad80fe15b74dcac1d8b6b98ae02a
jingwangian <jingwangian@gmail.com>
2019-12-02 11:45:01 +1100

    working version for flask

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
 import logging
 import connexion
-# from connexion.resolver import MethodViewResolver
 from flask_cors import CORS
 
 logging.basicConfig(level=logging.INFO)
 
+
 def hello():
-    print('hello: Welcome to forecast server')
-    return 'Welcome to forecast server'
+    print('hello: Welcome to related-media server')
+    return 'Welcome to related-media server'
+
 
 if __name__ == '__main__':
     app = connexion.FlaskApp(__name__, specification_dir='openapi/', debug=True)
 
     options = {"swagger_ui": True}
-    # app.add_api('forecast.yaml',
-    #             # options=options,
-    #             strict_validation=True, validate_responses=True )
-    app.add_api('swagger.yaml', 
+    app.add_api('swagger.yaml',
                 options=options,
                 arguments={'title': 'Hello World Example'})
 
